61fca01f62d0f06f991c4cc72deaa2be338ab4c3
badihaki <jojackblack@gmail.com>
2023-05-20 03:02:48 -0400

    Ground detection implemented in Player Movement

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -6,6 +6,8 @@ public class PlayerMovement : MonoBehaviour
 {
     private Player Player;
     private Rigidbody Rigidbody;
+    private Collider body;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,16 +18,65 @@ public class PlayerMovement : MonoBehaviour
     {
         Player = controller;
         Rigidbody = GetComponent<Rigidbody>();
+        body = transform.Find("Colliders").Find("Body").GetComponent<Collider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ground = Grounded();
     }
 
     public void MoveCharacter(Vector2 direction)
     {
         //
     }
+
+    public float groundRayDistance = 1.00f;
+    public bool Grounded()
+    {
+        Ray midRay = new Ray(transform.position, -transform.up);
+        Ray leftRay = new Ray(new Vector3(transform.position.x - body.bounds.extents.x, transform.position.y, transform.position.z), -transform.up);
+        Ray rightRay = new Ray(new Vector3(transform.position.x + body.bounds.extents.x, transform.position.y, transform.position.z), -transform.up);
+        Ray frontRay = new Ray(new Vector3(transform.position.x, transform.position.y, transform.position.z + body.bounds.extents.z), -transform.up);
+        Ray backRay = new Ray(new Vector3(transform.position.x, transform.position.y, transform.position.z - body.bounds.extents.z), -transform.up);
+        if (Physics.Raycast(midRay, out RaycastHit midHitInfo, groundRayDistance))
+        {
+            Debug.DrawLine(midRay.origin, new Vector3(midRay.origin.x, midRay.origin.y - groundRayDistance, midRay.origin.z), Color.red, 0.05f);
+            Debug.Log(midHitInfo);
+            return true;
+        }
+        if (Physics.Raycast(leftRay, out RaycastHit leftHitInfo, groundRayDistance))
+        {
+            Debug.DrawLine(leftRay.origin, new Vector3(leftRay.origin.x, leftRay.origin.y - groundRayDistance, leftRay.origin.z), Color.red, 0.05f);
+            return true;
+        }
+        if (Physics.Raycast(rightRay, out RaycastHit rightHitInfo, groundRayDistance))
+        {
+            Debug.DrawLine(rightRay.origin, new Vector3(rightRay.origin.x, rightRay.origin.y - groundRayDistance, rightRay.origin.z), Color.red, 0.05f);
+            return true;
+        }
+        if (Physics.Raycast(frontRay, out RaycastHit frontHitInfo, groundRayDistance))
+        {
+            Debug.DrawLine(frontRay.origin, new Vector3(frontRay.origin.x, frontRay.origin.y - groundRayDistance, frontRay.origin.z), Color.red, 0.05f);
+            return true;
+        }
+        if (Physics.Raycast(backRay, out RaycastHit backHitInfo, groundRayDistance))
+        {
+            Debug.DrawLine(backRay.origin, new Vector3(backRay.origin.x, backRay.origin.y - groundRayDistance, backRay.origin.z), Color.red, 0.05f);
+            return true;
+        }
+        else
+        {
+            Debug.DrawLine(midRay.origin, new Vector3(midRay.origin.x, midRay.origin.y - groundRayDistance, midRay.origin.z), Color.gray, 0.05f);
+            Debug.DrawLine(leftRay.origin, new Vector3(leftRay.origin.x, leftRay.origin.y - groundRayDistance, leftRay.origin.z), Color.gray, 0.05f);
+            Debug.DrawLine(rightRay.origin, new Vector3(rightRay.origin.x, rightRay.origin.y - groundRayDistance, rightRay.origin.z), Color.gray, 0.05f);
+            Debug.DrawLine(frontRay.origin, new Vector3(frontRay.origin.x, frontRay.origin.y - groundRayDistance, frontRay.origin.z), Color.gray, 0.05f);
+            Debug.DrawLine(backRay.origin, new Vector3(backRay.origin.x, backRay.origin.y - groundRayDistance, backRay.origin.z), Color.gray, 0.05f);
+            return false;
+        }
+    }
+    public bool ground;
+    
+// end
 }
