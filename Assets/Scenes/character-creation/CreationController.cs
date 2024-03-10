6b1de1d7d2afc9bde4722fcbb12b6e7f2a81b41e
badihaki <jojackblack@gmail.com>
2024-03-10 16:33:10 -0400

    figured out a workflow for importing characters. Both base and rags are set up with their own folders. Created tentative naming convention for outfit parts. Created a character customization screen

---
+++ Assets/Scenes/character-creation/CreationController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreationController : MonoBehaviour
+{
+    [field: SerializeField] private GameObject head;
+    [field: SerializeField] private GameObject[] _head_parts;
+    [field: SerializeField] private GameObject hair;
+    [field: SerializeField] private GameObject[] _hair_parts;
+    [field: SerializeField] private GameObject horns;
+    [field: SerializeField] private GameObject[] _horns_parts;
+    [field: SerializeField] private GameObject top;
+    [field: SerializeField] private GameObject[] _top_parts;
+    [field: SerializeField] private GameObject bottom;
+    [field: SerializeField] private GameObject[] _bottom_parts;
+    [field: SerializeField] private GameObject hands;
+    [field: SerializeField] private GameObject[] _hands_parts;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
