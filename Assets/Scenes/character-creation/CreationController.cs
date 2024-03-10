c1799f8809c476383fd1cef187dd3f2382e314dd
badihaki <jojackblack@gmail.com>
2024-03-10 18:35:00 -0400

    dynamically switching to different clothing set in code start func for CreationController.cs

--- Assets/Scenes/character-creation/CreationController.cs
+++ Assets/Scenes/character-creation/CreationController.cs
@@ -4,22 +4,28 @@ using UnityEngine;
 
 public class CreationController : MonoBehaviour
 {
-    [field: SerializeField] private GameObject head;
-    [field: SerializeField] private GameObject[] _head_parts;
-    [field: SerializeField] private GameObject hair;
-    [field: SerializeField] private GameObject[] _hair_parts;
-    [field: SerializeField] private GameObject horns;
-    [field: SerializeField] private GameObject[] _horns_parts;
-    [field: SerializeField] private GameObject top;
-    [field: SerializeField] private GameObject[] _top_parts;
-    [field: SerializeField] private GameObject bottom;
-    [field: SerializeField] private GameObject[] _bottom_parts;
-    [field: SerializeField] private GameObject hands;
-    [field: SerializeField] private GameObject[] _hands_parts;
+    [field: SerializeField] private SkinnedMeshRenderer head;
+    [field: SerializeField] private SkinnedMeshRenderer[] _head_parts;
+    [field: SerializeField] private SkinnedMeshRenderer hair;
+    [field: SerializeField] private SkinnedMeshRenderer[] _hair_parts;
+    [field: SerializeField] private SkinnedMeshRenderer horns;
+    [field: SerializeField] private SkinnedMeshRenderer[] _horns_parts;
+    [field: SerializeField] private SkinnedMeshRenderer top;
+    [field: SerializeField] private SkinnedMeshRenderer[] top_parts;
+    [field: SerializeField] private SkinnedMeshRenderer bottom;
+    [field: SerializeField] private SkinnedMeshRenderer[] bottom_parts;
+    [field: SerializeField] private SkinnedMeshRenderer hands;
+    [field: SerializeField] private SkinnedMeshRenderer[] _hands_parts;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // head = _head_parts[0];
+        // hair = _hair_parts[0];
+        // horns = _horns_parts[0];
+        // top = GameObject.Find("Character").transform.Find("Model").Find("Top").GetComponent<SkinnedMeshRenderer>();
+        top.sharedMesh = top_parts[1].sharedMesh;
+        // hands = _hands_parts[0];
+        bottom.sharedMesh = bottom_parts[1].sharedMesh;
     }
 
     // Update is called once per frame
