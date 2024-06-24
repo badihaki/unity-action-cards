e5252885f6667359c753197c93c874f31f5d5b23
badihaki <jojackblack@gmail.com>
2024-06-23 21:07:11 -0400

    code cleanup

--- Assets/Character/Scripts/Actor.cs
+++ Assets/Character/Scripts/Actor.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class Actor : MonoBehaviour
 {
     private Character Character;
+    [field: SerializeField] public Animator animationController { get; protected set; }
+
+    [field: SerializeField, Header("Animator Movement")]
+    public Vector3 animatorMovementVector { get; protected set; } = new Vector3();
+    [field: SerializeField] protected bool controlByRootMotion = false;
 
     public void Initialize(Character character)
     {
         Character = character;
+        animationController = GetComponent<Animator>();
     }
 }
