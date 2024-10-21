1ca07d0b1c68a1bd4efbdb8796327b8c488a88d4
B. Haki Martin <jojackblack@gmail.com>
2024-10-21 19:09:57 -0400

    added sword attack 2 animation

--- Assets/Cards/Weapons/1_Sword/MoveSet/SwordAttackAState.cs
+++ Assets/Cards/Weapons/1_Sword/MoveSet/SwordAttackAState.cs
@@ -8,4 +8,39 @@ public class SwordAttackAState : PlayerAttackSuperState
     public SwordAttackAState(PlayerCharacter pc, string animationName, PlayerStateMachine stateMachine) : base(pc, animationName, stateMachine)
     {
     }
+
+	public override void EnterState()
+	{
+		base.EnterState();
+		_PlayerCharacter._LocomotionController.ZeroOutVelocity();
+		_PlayerCharacter._AttackController.SetAttackParameters(2, 1.025f, 1.35f);
+		ShowOrHideWeapon(true);
+	}
+
+	public override void ExitState()
+	{
+		base.ExitState();
+		ShowOrHideWeapon(false);
+	}
+
+	public override void CheckStateTransitions()
+	{
+		if (canCombo && attackInput) _StateMachine.ChangeState(_PlayerCharacter._AttackController._AttackB);
+
+		// if (canCombo && specialInput) _StateMachine.ChangeState(_PlayerCharacter._AttackController._FinisherA);
+		// if (canCombo && jumpInput) _StateMachine.ChangeState(_PlayerCharacter._AttackController._LauncherAttack);
+
+		base.CheckStateTransitions();
+	}
+
+	public override void TriggerVisualEffect()
+	{
+		base.TriggerVisualEffect();
+		// Vector3 position = new Vector3(_PlayerCharacter._Actor.transform.position.x, _PlayerCharacter._Actor.transform.position.y + 1.75f, _PlayerCharacter._Actor.transform.position.z + 0.75f);
+		// Vector3 position = new Vector3(_PlayerCharacter._Actor.transform.position.x, _PlayerCharacter._AttackController._WeaponHolderR.transform.position.y, _PlayerCharacter._Actor.transform.position.z + 0.75f);
+		// Quaternion rotation = Quaternion.Euler(_PlayerCharacter._PlayerActor.transform.forward);
+
+		// GameObject vfx = Instantiate(_PlayerCharacter._AttackController._CurrentWeapon._WeaponAttackFX, position, rotation);
+		// vfx.transform.rotation = _PlayerCharacter._PlayerActor.transform.rotation;
+	}
 }
