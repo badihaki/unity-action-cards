3ef428d62b012f4851bdde8e6cb5aea252d1cac1
badihaki <jojackblack@gmail.com>
2024-11-12 13:37:56 -0500

    npc entities can track their aggressors, and correctly lose and gain aggression as needed.

--- Assets/Character/NPC/Scripts/NPCAggressionManager.cs
+++ Assets/Character/NPC/Scripts/NPCAggressionManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,8 +8,14 @@ public class NPCAggressionManager : MonoBehaviour
     public int _Aggression { get; private set; }
     [field: SerializeField]
     public List<Transform> _LastAggressors {  get; private set; }
+    [field: SerializeField]
+    private bool isAggressive;
 
-    public void Initialize(NonPlayerCharacter npc)
+    // waits
+    private WaitForSeconds slowWait = new WaitForSeconds(1.35f);
+    private WaitForSeconds fastWait = new WaitForSeconds(0.35f);
+
+	public void Initialize(NonPlayerCharacter npc)
     {
         _Aggression = 0;
         _LastAggressors = new List<Transform>();
@@ -17,9 +24,44 @@ public class NPCAggressionManager : MonoBehaviour
     public void AddAggression(int aggression, Transform aggressor)
     {
         _Aggression += aggression;
+        if (_Aggression >= 50 && !isAggressive)
+        {
+            isAggressive = true;
+            StartCoroutine(SlowlyLowerAggression());
+        }
+        if (_Aggression > 100) _Aggression = 100;
+
         if(!_LastAggressors.Contains(aggressor) && _LastAggressors.Count < 4)
         {
             _LastAggressors.Add(aggressor);
+        }
+    }
+
+    private IEnumerator SlowlyLowerAggression()
+    {
+        while(_Aggression >= 35)
+        {
+            yield return slowWait;
+            _Aggression -= 1;
+            print("slowly lose aggression");
+            yield return null;
+        }
+        StartCoroutine(QuicklyLowerAggression());
+    }
+    private IEnumerator QuicklyLowerAggression()
+    {
+        while (isAggressive)
+        {
+            yield return fastWait;
+            _Aggression -= 1;
+			print("quickly lose aggression");
+			if (_Aggression == 0)
+            {
+                _LastAggressors.Clear();
+                isAggressive = false;
+            }
+            else
+                yield return null;
         }
     }
 
