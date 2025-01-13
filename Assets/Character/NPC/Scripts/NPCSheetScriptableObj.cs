84e001d214dc2f5853aa73038e09bec846a859af
badihaki <jojackblack@gmail.com>
2025-01-12 23:20:35 -0500

    created an npc character sheet class that derives from character sheet. Recreated fred as an npc. Created character types for npcs, made wanderer type, and made fred a wanderer

---
+++ Assets/Character/NPC/Scripts/NPCSheetScriptableObj.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Char", menuName = "Characters/NPC/New NPC")]
+public class NPCSheetScriptableObj : CharacterSheet
+{
+    [Header("NPC Settings"), Tooltip("")]
+    public List<CharacterGroupScriptableObject> CharacterTypes;
+}
---
+++ Assets/Character/Scripts/CharacterType/CharacterGroupScriptableObject.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CharType",menuName = "Characters/New Character Group Type")]
+public class CharacterGroupScriptableObject : ScriptableObject
+{
+	public string GroupName;
+}
