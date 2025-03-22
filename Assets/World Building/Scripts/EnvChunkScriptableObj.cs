f7635de841fc8281895a96e4897f1b27319efa0d
badihaki <jojackblack@gmail.com>
2025-03-22 01:48:55 -0400

    expanded env chunk scriptable obj with border options and lists for elements.

--- Assets/World Building/Scripts/EnvChunkScriptableObj.cs
+++ Assets/World Building/Scripts/EnvChunkScriptableObj.cs
@@ -1,9 +1,71 @@
+#if UNITY_EDITOR
+using System.Collections.Generic;
+using UnityEditor;
+#endif
 using UnityEngine;
+using UnityEngine.UI;
 
+[CreateAssetMenu(fileName = "envChunk_", menuName = "WorldGen/Create new Environment Chunk")]
 public class EnvChunkScriptableObj : ScriptableObject
 {
 	[field: Header("Positioning Options"), SerializeField]
-	public bool isBorder;
-	[field: SerializeField]
-	public bool isCorner;
+	public bool isBorder { get; private set; }
+	[HideInInspector]
+	public bool isCorner { get; private set; }
+	[HideInInspector]
+	public bool north { get; private set; }
+	[HideInInspector]
+	public bool south { get; private set; }
+	[HideInInspector]
+	public bool east { get; private set; }
+	[HideInInspector]
+	public bool west { get; private set; }
+
+	[field: SerializeField, Header("Env building")]
+	public GameObject chunkGameObj { get; private set; }
+	public List<GameObject> buildingTemplates { get; private set; }
+	public List<GameObject> floraTemplates { get; private set; }
+	public List<NonPlayerCharacter> regularNPCs { get; private set; }
+	public List<NonPlayerCharacter> specialNPCs { get; private set; }
+	public List<NonPlayerCharacter> enemies { get; private set; }
+	public List<NonPlayerCharacter> specialEnemies { get; private set; }
+
+
+	// util class
+#if UNITY_EDITOR
+	[CustomEditor(typeof(EnvChunkScriptableObj))]
+	public class EnvChunkEditorExtension : Editor
+	{
+		// editor ext here
+		public override void OnInspectorGUI()
+		{
+			EnvChunkScriptableObj script = (EnvChunkScriptableObj)target;
+
+			script.isBorder = EditorGUILayout.Toggle("Is a Border?", script.isBorder);
+
+			if (script.isBorder)
+			{
+				//DrawPropertiesExcluding(serializedObject, "isCorner");
+				script.isCorner = false;
+				DrawBorderToggles(script);
+			}
+			else if (script.isBorder == false)
+			{
+				script.isCorner = EditorGUILayout.Toggle("Is Corner?", script.isCorner);
+				if (script.isCorner)
+				{
+					DrawBorderToggles(script);
+				}
+			}
+		}
+
+		private static void DrawBorderToggles(EnvChunkScriptableObj script)
+		{
+			script.north = EditorGUILayout.Toggle("North Border", script.north);
+			script.south= EditorGUILayout.Toggle("South Border", script.south);
+			script.east= EditorGUILayout.Toggle("East Border", script.east);
+			script.west= EditorGUILayout.Toggle("West Border", script.west);
+		}
+	}
+#endif
 }
