68b7cd77bef36b8effe36d09f6cb378297884a9d
B. Haki Martin <jojackblack@gmail.com>
2025-04-07 20:47:20 -0400

    CardBtn now adds an event listener to interface with UI canvas. UI canvas has a method that can support this.

--- Assets/UI/Interaction/Card Management/CardDeckManagementBtn.cs
+++ Assets/UI/Interaction/Card Management/CardDeckManagementBtn.cs
@@ -1,14 +1,32 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CardDeckManagementBtn : MonoBehaviour
 {
 	private CardScriptableObj cardScriptableObj;
 	private CardManagementCanvas cardManagementCanvas;
+	public Button btn { get; private set; }
+	private int btnId;
 
-	public void Initialize(CardScriptableObj _cardScrObj, CardManagementCanvas _cardCanvas)
+	public void Initialize(CardScriptableObj _cardScrObj, CardManagementCanvas _cardCanvas, int _btnId)
 	{
 		cardScriptableObj = _cardScrObj;
 		cardManagementCanvas = _cardCanvas;
+		btnId = _btnId;
+
+		btn = GetComponent<Button>();
+		btn.onClick.AddListener(() => cardManagementCanvas.ClickedCardIcon(btnId));
+
+		TextMeshProUGUI text = transform.Find("Text").GetComponent<TextMeshProUGUI>();
+		text.text = cardScriptableObj._CardName;
+
+		GetComponent<Image>().sprite = cardScriptableObj._CardImage;
+	}
+
+	private void OnDestroy()
+	{
+		btn.onClick.RemoveAllListeners();
 	}
 
 	// end
--- Assets/UI/Scripts/CardManagementCanvas.cs
+++ Assets/UI/Scripts/CardManagementCanvas.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CardManagementCanvas : InteractionCanvasBase
@@ -8,6 +10,8 @@ public class CardManagementCanvas : InteractionCanvasBase
 	public GameObject managementGroup { get; private set; }
 	[field: SerializeField]
 	public GameObject cardContentArea { get; private set; }
+	[SerializeField]
+	private List<CardDeckManagementBtn> cardButtons;
 
 	public override void Initialize(Interaction interaction, string interactionName)
 	{
@@ -24,14 +28,45 @@ public class CardManagementCanvas : InteractionCanvasBase
 
 		basePanel.SetActive(false);
 		managementGroup.SetActive(true);
+		BuildContentArea();
 	}
+
+	private void BuildContentArea()
+	{
+   //     foreach (CardStruct cardStruct in GameManagerMaster.GameMaster.CardsManager.cardsFound)
+   //     {
+			//CardDeckManagementBtn btn = Instantiate(cardDeckManagementBtnTemplate, cardContentArea.transform);
+			//btn.Initialize(cardStruct.CardScriptableObj, this);
+			//cardButtons.Add(btn);
+   //     }
+		for (int i = 0; i < GameManagerMaster.GameMaster.CardsManager.cardsFound.Count; i++)
+		{
+			CardStruct cardStruct = GameManagerMaster.GameMaster.CardsManager.cardsFound[i];
+			CardDeckManagementBtn btn = Instantiate(cardDeckManagementBtnTemplate, cardContentArea.transform);
+			btn.Initialize(cardStruct.CardScriptableObj, this, i);
+			cardButtons.Add(btn);
+		}
+    }
+
+	private void DestroyContentArea()
+	{
+        foreach (CardDeckManagementBtn card in cardButtons)
+        {
+            Destroy(card.gameObject);
+        }
+		cardButtons.Clear();
+    }
 
 	public override void CancelInteraction()
 	{
 		basePanel.SetActive(true);
 		managementGroup.SetActive(false);
-
+		DestroyContentArea();
 		base.CancelInteraction();
+	}
+	public void ClickedCardIcon(int btnId)
+	{
+		print($"clicking card {GameManagerMaster.GameMaster.CardsManager.cardsFound[btnId].CardScriptableObj._CardName}");
 	}
 
 	// end
